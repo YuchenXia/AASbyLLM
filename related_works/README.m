0bbf7188270881b9a040556d2bd7d5a058d3399b
YuchenXia <39265101+YuchenXia@users.noreply.github.com>
2024-01-02 12:01:49 +0100

    Update README.m

--- related_works/README.m
+++ related_works/README.m
@@ -1,4 +1,23 @@
+#The main features of this work:
+First research prototype applying **generative LLM** for generating of AAS
+Generative LLM (inferencing) and embedding LLM (similarity search)
+
+Knowledge stored in LLM instead of manually crafted mapping rules
+Wider range of **input flexibility** and format independent
+
+The system add proper **semantic annotation** for disambiguating the **concepts of the data property**
+The system is able to optimize relevant details for **better quality** of AAS 
+
+This prototype demonstrate the machine capability to **semantically understand and generate data properties**.
+This capability can enable:
+  1) an **interoperable** information exchange in context of **digitalization**
+  2) and higher degree of **task automation** in context of **autonomization**
+
 List of Related Works
+
+These works apply embedding language models for information processing:
+
+
 
 1.
 2.
